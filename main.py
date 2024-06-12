932bc92bc64685dc288500f04f1335fec6341ca1
Awan <itproject2k22@gmail.com>
2024-06-13 00:25:41 +0700

    push code

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Customer(Agent):
         step_direction = (int(direction_vector[0] / max(1, abs(direction_vector[0]))),
                           int(direction_vector[1] / max(1, abs(direction_vector[1]))))
         new_position = (current_pos[0] + step_direction[0], current_pos[1] + step_direction[1])
-        if not self.model.grid.out_of_bounds(new_position) and new_position not in self.model.boundaries:
+        if not self.model.grid.out_of_bounds(new_position) and new_position not in self.model.boundaries and new_position not in self.model.big_table and new_position not in self.model.store:
             if self.state != "eating" or self.model.grid.is_cell_empty(new_position):
                 self.model.grid.move_agent(self, new_position)
     
@@ -114,18 +114,20 @@ class CanteenModel(Model):
 
         self.steps_since_last_customer = 0
 
-        self.entry_point = (43, random.randint(16, 18))
+        self.entry_point = (41, random.randint(0, 22))
         self.cashier_location = [(2, 19), (6, 19), (10, 19), (14, 19), (18, 19), (22, 19), (3, 19), (7, 19), (11, 19),
                                  (15, 19), (19, 19), (23, 19), (4, 19), (8, 19), (12, 19), (16, 19), (20, 19),
                                  (24, 19), (5, 19), (9, 19), (13, 19), (17, 19), (21, 19), (25, 19)]
-        self.exit_location = [(43, i) for i in range(1,23)]
+        self.exit_location = [(43, i) for i in range(1, 23)]
+        self.store = [(i+2, 20) for i in range(24)] + [(i+2, 21) for i in range(24)] + [(i+2, 22) for i in range(24)]
 
         self.dining_areas = [(3 * (i + 1), 14) for i in range(11)] + [(3 * (j + 1) + 1, 14) for j in range(11)] + \
                             [(3 * (i + 1), 15) for i in range(11)] + [(3 * (j + 1) + 1, 15) for j in range(11)] + \
                             [(3 * (i + 1), 10) for i in range(11)] + [(3 * (j + 1) + 1, 10) for j in range(11)] + \
                             [(3 * (i + 1), 11) for i in range(11)] + [(3 * (j + 1) + 1, 11) for j in range(11)] + \
-                            [(33, 2), (33, 3), (34, 2), (34, 3), (33, 19), (34, 19), (33, 20), (34, 20), (31, 19),
-                             (30, 19), (31, 20), (30, 20)] + \
+                            [(33, 2), (33, 3), (34, 2), (34, 3),
+                             (33, 5), (33, 6), (34, 5), (34, 6),(33, 20), (34, 20), (33, 21), (34, 21), (31, 20),
+                             (30, 20), (31, 21), (30, 21)] + \
                             [(6, i + 3) for i in range(4)] + [(7, 3), (7, 6)] + [(8, 3), (8, 6)] + [(9, i + 3) for i in
                                                                                                       range(4)] + \
                             [(13, i + 3) for i in range(4)] + [(14, 3), (14, 6)] + [(15, 3), (15, 6)] + [(16, i + 3) for i
@@ -137,9 +139,9 @@ class CanteenModel(Model):
                             [(27, i + 3) for i in range(4)] + [(28, 3), (28, 6)] + [(29, 3), (29, 6)] + [(30, i + 3) for i
                                                                                                           in range(
                                 4)]
-
-        self.boundaries = [(i, 0) for i in range(width)] + [(i, 23) for i in range(width)] + \
-                          [(0, j) for j in range(height)]
+        self.big_table = [(7, 4), (7, 5), (8, 4), (8, 5)] + [(14, 4), (14, 5), (15, 4), (15, 5)] + [(21, 4), (21, 5), (22, 4), (22, 5)] + [(28, 4), (28, 5), (29, 4), (29, 5)]
+        self.boundaries = [(i, 0) for i in range(width)] + [(i, 23) for i in range(44)] +\
+                          [(0, j) for j in range(height)] 
 
         self.next_customer_time = self.random_time_between(0, 60)
 
@@ -162,6 +164,14 @@ class CanteenModel(Model):
 
         for exit_loc in self.exit_location:
             self.add_static_element(exit_loc, "exit_location")
+        
+        for store_loc in self.store:
+            self.add_static_element(store_loc, "store")
+            
+        for big_table_loc in self.big_table:
+            self.add_static_element(big_table_loc, "big_table")
+        
+        
 
     def add_static_element(self, pos, element_type):
         element = StaticElement(self.next_id(), self, element_type)
@@ -218,7 +228,7 @@ class CanteenModel(Model):
             if self.current_time >= self.next_customer_time:
                 self.add_customer()
 
-        self.current_time += 1 / 120  # Assume one step is one minute
+        self.current_time += 1 / 60  # Assume one step is one minute
 
         if self.current_time >= self.end_time:
             self.running = False
@@ -277,14 +287,22 @@ def customer_portrayal(agent):
         if agent.element_type == "boundary":
             portrayal["Color"] = "black"
             portrayal["Layer"] = 0
+        elif agent.element_type == "cashier_location":
+            portrayal["Color"] = "lightyellow"
+            portrayal["Layer"] = 0
         elif agent.element_type == "dining_area":
             portrayal["Color"] = "lightblue"
             portrayal["Layer"] = 0
         elif agent.element_type == "exit_location":
             portrayal["Color"] = "orange"
             portrayal["Layer"] = 0
+        elif agent.element_type == "store":
+            portrayal["Color"] = "yellow"
+            portrayal["Layer"] = 0
+        elif agent.element_type == "big_table":
+            portrayal["Color"] = "blue"
+            portrayal["Layer"] = 0
         return portrayal
-
 
 
 width, height = 44, 24
@@ -301,7 +319,7 @@ server = ModularServer(CanteenModel,
                        "Canteen Model",
                        {"width": width, "height": height})
 
-server.port = 8603
+server.port = 8521
 
 if __name__ == "__main__":
     server.launch()
